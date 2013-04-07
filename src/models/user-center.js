d88ef8df0ca75f40bc93fe6d6210c3935c6ef4bc
Michael.Lee <chunmin.li@sh.leoburnett.com>
2013-04-07 09:34:12 +0800

    [dev]fix the bug for nodejs 0.10

--- src/models/user-center.js
+++ src/models/user-center.js
@@ -77,11 +77,9 @@ UserCenter.hash = function(pwd, salt, fn){
  */
 UserCenter.authenticate = function(input, pass, salt, fn){
   UserCenter.hash(input, salt, function(err, hash){
-    console.log('*************************************');
-    console.log(pass);
-    console.log(hash);
     if (err) return fn(err);
-    if (pass == hash) return fn(null, true);
+    // if (pass == hash) return fn(null, true);
+    if (pass == hash || pass.buffer.toString() == hash.toString()) return fn(null, true);
 
     return fn(new InvalidPasswordError());
   });
