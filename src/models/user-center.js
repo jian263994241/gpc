753f9b5e139fddae3b8a5199f3da8a2eec9f62ff
Michael.Lee <chunmin.li@sh.leoburnett.com>
2013-05-15 09:28:24 +0800

    [dev]remove any ssl verification

--- src/models/user-center.js
+++ src/models/user-center.js
@@ -145,10 +145,10 @@ UserCenter.getResetLink = function(email, config, fn){
 
     var userInfo = data[0];
     var server = emailServer.server.connect({
-      user: config["admin_email"],
-      password: config["admin_email_password"],
+      // user: config["admin_email"],
+      // password: config["admin_email_password"],
       host: config["admin_email_host"],
-      ssl: true
+      // ssl: true
     });
 
     server.send({
